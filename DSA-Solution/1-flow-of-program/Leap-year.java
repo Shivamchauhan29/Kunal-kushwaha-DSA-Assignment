5210d9ede1d5fcd840ace141f3862f7db0c6b052
Shivam Chauhan <81063248+Shivamchauhan29@users.noreply.github.com>
2021-09-02 23:40:51 +0530

    Update Leap-year.java

--- DSA-Solution/1-flow-of-program/Leap-year.java
+++ DSA-Solution/1-flow-of-program/Leap-year.java
@@ -1,1 +1,42 @@
 
+import java.util.Scanner;
+
+// Input a year and find whether it is a leap year or not.
+
+public class Leap_year{
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Please enter a year : ");
+        int year = in.nextInt();
+        
+        boolean ans = checkyear(year);
+
+        if (ans == true) {
+            System.out.println(year + " is a Leap Year.");
+        }
+        else{
+            System.out.println(year  +" is not a leap year.");
+        }
+
+
+        in.close();
+    }
+
+    static boolean checkyear(int year){
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+        }
+        else{
+            if (year % 4 == 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
