d14dd07996bae2d62b4a753434f242e57b06e169
Shivam Chauhan <81063248+Shivamchauhan29@users.noreply.github.com>
2021-09-03 12:57:09 +0530

    Create 2.Area_Triangle.java

---
+++ DSA-Solution/3.Conditional-loops/2.Area_Triangle.java
@@ -0,0 +1,20 @@
+// Area Of Triangle Java Program
+
+import java.util.Scanner;
+
+public class Area_Triangle {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the base and height of the triangle :");
+
+        float base = in.nextFloat();
+        float height = in.nextFloat();
+
+        float area = (base * height) / 2;
+        System.out.println("The area of the triangle = "+area);
+
+        in.close();
+    }
+}
